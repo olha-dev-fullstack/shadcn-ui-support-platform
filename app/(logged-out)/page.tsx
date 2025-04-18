603ee4899dc414caa07119b26b0a619587044261
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-04-18 14:57:05 +0300

    init project

---
+++ app/(logged-out)/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <>
+      <div>Support Me</div>
+      <Button>Log in</Button>
+      <Button>Sign up</Button>
+    </>
+  );
+};
+
+export default LandingPage;
