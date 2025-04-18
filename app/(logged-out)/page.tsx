3822811be664ab1725f6ad524779d3ffd53f3128
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-04-18 15:11:43 +0300

    layout start page

--- app/(logged-out)/page.tsx
+++ app/(logged-out)/page.tsx
@@ -1,12 +1,25 @@
 import { Button } from "@/components/ui/button";
+import { PersonStandingIcon } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 const LandingPage = () => {
   return (
     <>
-      <div>Support Me</div>
-      <Button>Log in</Button>
-      <Button>Sign up</Button>
+      <h1 className="flex gap-2 items-center">
+        <PersonStandingIcon size={50} className="text-pink-500" />
+        Support Me
+      </h1>
+      <p>The best dashboard to manage customer support</p>
+      <div className="flex gap-2 items-center">
+        <Button asChild>
+          <Link href="/login">Log in</Link>
+        </Button>
+        <small>or</small>
+        <Button variant={"outline"}>
+          <Link href="/sign-up">Sign up</Link>
+        </Button>
+      </div>
     </>
   );
 };
