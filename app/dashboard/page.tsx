fff7838c710f9f7e2d16ea0d5add394143e1edc1
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-04-21 17:09:09 +0300

    add employees stats containers and detail page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,22 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import React from "react";
+import EmployeesStats from "./components/employees/employees-stats";
 
 const DashboardPage = () => {
   return (
-    <>
-    
-      <div>main content area</div>
-    </>
+    <Tabs defaultValue="employees">
+    <TabsList className="mb-4">
+      <TabsTrigger value="employees">Employees stats</TabsTrigger>
+      <TabsTrigger value="teams">Teams stats</TabsTrigger>
+    </TabsList>
+    <TabsContent value="employees">
+      <EmployeesStats />
+    </TabsContent>
+    <TabsContent value="teams">
+      {/* <TeamsStats /> */}
+    </TabsContent>
+  </Tabs>
+
   );
 };
 
